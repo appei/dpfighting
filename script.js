06838125f69b5ca95e7dbfd846ac6a9483f729a5
appei <appuufish@gmail.com>
2022-11-28 23:09:11 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,307 @@
+const imageattArray = [
+"https://i.imgur.com/eOGLWvJ.png",
+"https://i.imgur.com/hF88bN7.png",
+"https://i.imgur.com/0mt1qtp.png",
+"https://i.imgur.com/R4Jrpe0.png",
+"https://i.imgur.com/VRnSFxw.png",
+"https://i.imgur.com/d8zTs0r.png",
+"https://i.imgur.com/xMmrtq7.png",
+"https://i.imgur.com/vocxmzH.png",
+"https://i.imgur.com/u4DA8Y5.png",
+"https://i.imgur.com/qGIgn8v.png",
+"https://i.imgur.com/YM278Qb.png",
+"https://i.imgur.com/kTjfjMp.png",
+"https://i.imgur.com/seWCkUd.png",
+"https://i.imgur.com/jUl5Dyb.png",
+"https://i.imgur.com/oY7Aut9.png",
+"https://i.imgur.com/bOEDgjb.png",
+"https://i.imgur.com/uJk0H60.png",
+"https://i.imgur.com/YoTNrsV.png",
+"https://i.imgur.com/ootgVVW.png",
+"https://i.imgur.com/haFbJCQ.png",
+"https://i.imgur.com/5xhsuCg.png",
+"https://i.imgur.com/FpCDIZM.png",
+"https://i.imgur.com/n7jAyLp.png",
+"https://i.imgur.com/vRvrTcK.png",
+"https://i.imgur.com/2N41S80.png",
+"https://i.imgur.com/wn1MWjg.png",
+"https://i.imgur.com/N66GynB.png",
+"https://i.imgur.com/L4EoA45.png",
+"https://i.imgur.com/BaVgSiR.png",
+"https://i.imgur.com/lLlHoro.png",
+"https://i.imgur.com/V3TRCib.png",
+"https://i.imgur.com/KZEqtsP.png",
+"https://i.imgur.com/PjJTaET.png",
+"https://i.imgur.com/WUDggXN.png",
+"https://i.imgur.com/9RqhHoU.png",
+"https://i.imgur.com/IugvmWN.png",
+"https://i.imgur.com/23GVgJQ.png",
+"https://i.imgur.com/4pj1q4f.png",
+"https://i.imgur.com/FCwTHIO.png",
+"https://i.imgur.com/lHXgUCj.png",
+"https://i.imgur.com/cVIRi5r.png",
+"https://i.imgur.com/d7LNU5R.png",
+"https://i.imgur.com/xrdtAdD.png",
+"https://i.imgur.com/b8zG9C5.png",
+"https://i.imgur.com/2hFgWrT.png",
+"https://i.imgur.com/a10Mjo4.png",
+"https://i.imgur.com/jTBs30O.png",
+"https://i.imgur.com/PMeZIda.png",
+"https://i.imgur.com/1JHjn65.png",
+"https://i.imgur.com/oAdoLjR.png",
+"https://i.imgur.com/kAZPRl8.png",
+"https://i.imgur.com/gXLu4ab.png",
+"https://i.imgur.com/IIwlO1v.png",
+"https://i.imgur.com/QJUhGmI.png",
+"https://i.imgur.com/YHvJsY1.png",
+"https://i.imgur.com/LrfIVF2.png",
+"https://i.imgur.com/xS5TTNl.png",
+"https://i.imgur.com/mfo4TA5.png",
+"https://i.imgur.com/twdBo52.png",
+"https://i.imgur.com/F6iRrS3.png",
+"https://i.imgur.com/6R0l9FD.png",
+"https://i.imgur.com/wO9o8cI.png",
+"https://i.imgur.com/sgVzqsl.png",
+"https://i.imgur.com/h8gNGzh.png",
+"https://i.imgur.com/6vo9WHR.png",
+"https://i.imgur.com/6JKvxqh.png",
+"https://i.imgur.com/nBuCIY4.png",
+"https://i.imgur.com/Fvn645k.png",
+"https://i.imgur.com/3AGD5de.png",
+"https://i.imgur.com/1Sa1tUB.png",
+"https://i.imgur.com/B6gsMGR.png",
+"https://i.imgur.com/ZHnRpcy.png",
+"https://i.imgur.com/NyLqICX.png",
+"https://i.imgur.com/GeLg57L.png",
+"https://i.imgur.com/CqMKLt8.png",
+"https://i.imgur.com/4GdhQFR.png",
+"https://i.imgur.com/KkgNpgW.png",
+"https://i.imgur.com/ttjwdYc.png",
+"https://i.imgur.com/efm2v2u.png",
+"https://i.imgur.com/ApZYGX7.png",
+"https://i.imgur.com/sYjsXZ2.png",
+"https://i.imgur.com/B2gDWq3.png",
+"https://i.imgur.com/54wU5KA.png",
+"https://i.imgur.com/RcNHNNA.png",
+"https://i.imgur.com/5Q1PmZj.png",
+"https://i.imgur.com/aLklOUG.png",
+"https://i.imgur.com/KFpuxbB.png",
+"https://i.imgur.com/n1qMGsl.png",
+"https://i.imgur.com/DK9MbWR.png",
+"https://i.imgur.com/nQiRNRQ.png",
+"https://i.imgur.com/m9c6VVS.png",
+"https://i.imgur.com/VaUVnK7.png",
+"https://i.imgur.com/M232mdB.png",
+"https://i.imgur.com/mpWdQrB.png",
+"https://i.imgur.com/h9pr7gs.png",
+"https://i.imgur.com/hiAgs9w.png",
+"https://i.imgur.com/CEYFztU.png",
+"https://i.imgur.com/GfcI6fU.png",
+"https://i.imgur.com/GCkdIzu.png",
+"https://i.imgur.com/rBc0W2J.png",
+"https://i.imgur.com/IBasskv.png",
+"https://i.imgur.com/Fi8h8oK.png",
+"https://i.imgur.com/MqiBXHO.png",
+"https://i.imgur.com/3MdukGd.png",
+"https://i.imgur.com/sYbZZub.png",
+"https://i.imgur.com/VwDsZ7e.png",
+"https://i.imgur.com/nQWg608.png",
+"https://i.imgur.com/MyQSRjZ.png",
+"https://i.imgur.com/xONiYAW.png",
+"https://i.imgur.com/2lQJbKD.png"
+  ];
+const imageDefArray = [
+"https://i.imgur.com/71mwx5O.png",
+"https://i.imgur.com/GR90R2J.png",
+"https://i.imgur.com/9ij166l.png",
+"https://i.imgur.com/r2yMv1q.png",
+"https://i.imgur.com/IcARhVo.png",
+"https://i.imgur.com/fKw0Qa8.png",
+"https://i.imgur.com/APrFIX8.png",
+"https://i.imgur.com/m3fjAV2.png",
+"https://i.imgur.com/2sFsE5C.png",
+"https://i.imgur.com/ZFOdMnI.png"
+  ];
+const imagelucArray = [
+"https://i.imgur.com/bVlXcYz.png",
+"https://i.imgur.com/Qo2RYNl.png",
+"https://i.imgur.com/jWGfW5J.png",
+"https://i.imgur.com/bVlXcYz.png",
+"https://i.imgur.com/Qo2RYNl.png",
+"https://i.imgur.com/jWGfW5J.png",
+"https://i.imgur.com/bVlXcYz.png",
+"https://i.imgur.com/Qo2RYNl.png",
+"https://i.imgur.com/jWGfW5J.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png",
+"https://i.imgur.com/RF8ONze.png"
+  ];
+const imagefcsArray = [
+"https://i.imgur.com/pLTSZ8d.png",
+"https://i.imgur.com/pLTSZ8d.png",
+"https://i.imgur.com/pLTSZ8d.png",
+"https://i.imgur.com/pLTSZ8d.png",
+"https://i.imgur.com/Kupuxs7.png",
+"https://i.imgur.com/CVKMoxl.png",
+"https://i.imgur.com/yTC5wyf.png",
+"https://i.imgur.com/m5bMjN9.png",
+"https://i.imgur.com/CpZJz2g.png",
+"https://i.imgur.com/Kupuxs7.png",
+"https://i.imgur.com/CVKMoxl.png",
+"https://i.imgur.com/yTC5wyf.png",
+"https://i.imgur.com/m5bMjN9.png",
+"https://i.imgur.com/CpZJz2g.png"
+  ];
+
+const att_btn = document.getElementById('att_btn');
+const att_result = document.getElementById('att_result');
+
+const def_btn = document.getElementById('def_btn');
+const def_result = document.getElementById('def_result');
+
+const lck_btn = document.getElementById('lck_btn');
+const lck_result = document.getElementById('lck_result');
+
+const fcs_btn = document.getElementById('fcs_btn');
+const fcs_result = document.getElementById('fcs_result');
+
+att_btn.addEventListener('click', () => fetchAttackImage(imageattArray));
+
+def_btn.addEventListener('click', () => fetchDefendImage(imageDefArray));
+
+lck_btn.addEventListener('click', () => fetchLuckImage(imagelucArray));
+
+fcs_btn.addEventListener('click', () => fetchFocusImage(imagefcsArray));
+//
+function fetchAttackImage(array) {
+  let randomNum = Math.floor(Math.random() * imageattArray.length);
+  
+  temp = array[randomNum];
+  temp2 = '<img src=' + temp + '>';
+  console.log(temp);
+  att_result.innerHTML = temp2;
+}
+function fetchDefendImage(array) {
+  let randomNum = Math.floor(Math.random() * imageDefArray.length);
+  
+  temp = array[randomNum];
+  temp2 = '<img src=' + temp + '>';
+  console.log(temp);
+  def_result.innerHTML = temp2;
+}
+
+
+function fetchLuckImage(array) {
+  let randomNum = Math.floor(Math.random() * imagelucArray.length);
+  
+  temp = array[randomNum];
+  temp2 = '<img src=' + temp + '>';
+  console.log(temp);
+  lck_result.innerHTML = temp2;
+}
+
+function fetchFocusImage(array) {
+  let randomNum = Math.floor(Math.random() * imagefcsArray.length);
+  
+  temp = array[randomNum];
+  temp2 = '<img src=' + temp + '>';
+  console.log(temp);
+  fcs_result.innerHTML = temp2;
+}
